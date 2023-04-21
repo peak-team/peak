f1e79648ebb83d3e7becdb872e725e50dfc98fe6
Ian Wang <yinzhi.wang.cug@gmail.com>
2023-04-21 15:06:58 -0500

    add more details to the document

--- include/pthread_listener.h
+++ include/pthread_listener.h
@@ -43,12 +43,29 @@ struct _PthreadState {
 };
 
 /**
- * @brief Attach the pthread listener
+ * @brief Attaches a PthreadListener to intercept calls to pthread_create and tracks newly created threads
+ *
+ * This function initializes a new thread id mapping hash table, inserts the main thread into the hash table,
+ * creates a GumInterceptor object and a PthreadListener object, and attaches the listener to the pthread_create function
+ * with the GumInterceptor object. Once this function is called, newly created threads will be tracked by PthreadListener
+ * until the pthread_listener_dettach function is called.
+ * 
+ * When a new thread is created by pthread_create, the listener intercepts the call and assigns a new unique 
+ * ID to the child thread, adds the ID to the hash table, and increments the counter for the next unique ID. 
+ * The hash table is later used to retrieve the ID of a thread given its pthread_t identifier, allowing other 
+ * parts of the program to keep track of the created threads.
+ *
+ * @return void
  */
 void pthread_listener_attach();
 
 /**
- * @brief Detach the pthread listener
+ * @brief Detaches the PthreadListener and cleans up related resources
+ *
+ * This function detaches the PthreadListener from the pthread_create function and releases related resources
+ * including the thread id mapping hash table, the GumInterceptor object, and the PthreadListener object.
+ *
+ * @return void
  */
 void pthread_listener_dettach();
 
