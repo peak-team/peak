b39b0b50f2c203ecb53feb4809e8441b5adccd81
Ian Wang <yinzhi.wang.cug@gmail.com>
2023-04-23 08:56:53 -0500

    use attribute to prevent inline

--- test/sleep/test_sleep.c
+++ test/sleep/test_sleep.c
@@ -6,18 +6,10 @@
 
 #define N 80
 
-float my_sleep_func()
+__attribute__((noinline)) float my_sleep_func()
 {
-    float x[N], y[N];
-    for (int i = 0; i < N; i++) {
-        x[i] = i;
-        y[i] = i + 1;
-    }
-    // printf("y[N]: %f\n", y[N-1]);
-    if(x[N-1] + y[N-1]) {
-        struct timespec ts = { 0, 10000000 }; // Sleep for 0.01 seconds (1,000,000 nanoseconds)
-        nanosleep(&ts, NULL);
-    }
+    struct timespec ts = { 0, 10000000 }; // Sleep for 0.01 seconds (1,000,000 nanoseconds)
+    nanosleep(&ts, NULL);
     return 0.01;
 }
 
