59b0ca466f05bb99b63e874c9850e26cfef13027
tenngenn <1506211418@qq.com>
2025-03-05 19:47:07 -0600

    add heartbeat

---
+++ test/heartbeat/test_heartbeat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+__attribute__((noinline)) float my_sleep_func()
+{
+    struct timespec ts = { 0, 100000 }; // Sleep for 0.0001 seconds (10,000,000 nanoseconds)
+    nanosleep(&ts, NULL);
+    return 0.01;
+}
+
+int main()
+{
+    float total = 0.0;
+#pragma omp parallel for reduction(+ : total)
+    for (int i = 0; i < 1000; i++) {
+        total += my_sleep_func();
+    }
+    printf("Sleep is done: %f\n", total);
+    return 0;
+}
