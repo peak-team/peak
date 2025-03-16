55fb6ceb25b0301f049189e00e7d7d3d6d98fc88
weichenhuang <weichenhuang@login1.vista.tacc.utexas.edu>
2025-03-16 15:42:51 -0500

    fix: [CUDA] fix mpi type and kernel dim info unsigned type

--- src/cuda_interceptor.c
+++ src/cuda_interceptor.c
@@ -17,19 +17,18 @@ static CUresult (*original_cu_launch_kernel)(
     unsigned int sharedMemBytes, CUstream hStream, void** kernelParams, void** extra);
 
 typedef struct {
-    // TODO: is int a good range?
-    int total_gpu_threads;
-    int max_gpu_threads;
-    int min_gpu_threads;
-    int total_kernel_call_cnt;
-    int max_kernel_call_cnt;
-    int min_kernel_call_cnt;
-    int total_block_size;
-    int max_block_size;
-    int min_block_size;
-    int total_grid_size;
-    int max_grid_size;
-    int min_grid_size;
+    gulong total_gpu_threads;
+    gulong max_gpu_threads;
+    gulong min_gpu_threads;
+    gulong total_kernel_call_cnt;
+    gulong max_kernel_call_cnt;
+    gulong min_kernel_call_cnt;
+    gulong total_block_size;
+    gulong max_block_size;
+    gulong min_block_size;
+    gulong total_grid_size;
+    gulong max_grid_size;
+    gulong min_grid_size;
 } KernelDimInfo;
 
 gboolean str_equal_function(gconstpointer a, gconstpointer b) {
@@ -40,13 +39,13 @@ static cudaError_t peak_cuda_launch_kernel(
     const void* func, dim3 gridDim, dim3 blockDim,
     void** args, size_t sharedMem, cudaStream_t stream)
 {
-    gchar* kernel_name = gum_symbol_name_from_address(func);
+    gchar* kernel_name = gum_symbol_name_from_address((gpointer)func);
     if (kernel_name != NULL) {
         g_mutex_lock(&cuda_kernel_local_dim_mapping_mutex);
         
-        int total_threads = (gridDim.x * blockDim.x) * (gridDim.y * blockDim.y) * (gridDim.z * blockDim.z);
-        int grid_size = gridDim.x * gridDim.y * gridDim.z;
-        int block_size = blockDim.x * blockDim.y * blockDim.z;
+        gulong total_threads = (gridDim.x * blockDim.x) * (gridDim.y * blockDim.y) * (gridDim.z * blockDim.z);
+        gulong grid_size = gridDim.x * gridDim.y * gridDim.z;
+        gulong block_size = blockDim.x * blockDim.y * blockDim.z;
         KernelDimInfo* dim_info = g_hash_table_lookup(cuda_kernel_local_dim_mapping, g_strdup(kernel_name));
         if (!dim_info) {
             dim_info = g_new(KernelDimInfo, 1);
@@ -88,14 +87,15 @@ static CUresult peak_cu_launch_kernel(
     unsigned int blockDimX, unsigned int blockDimY, unsigned int blockDimZ,
     unsigned int sharedMemBytes, CUstream hStream, void** kernelParams, void** extra)
 {
-    // TODO: mentioned in comment https://forums.developer.nvidia.com/t/how-to-get-a-kernel-functions-name-through-its-pointer/37427/2
+    // Kernel Name Address Source
+    // https://forums.developer.nvidia.com/t/how-to-get-a-kernel-functions-name-through-its-pointer/37427/2
     gchar* kernel_name = *(char**)((size_t)func + 8);
     if (kernel_name != NULL) {
         g_mutex_lock(&cuda_kernel_local_dim_mapping_mutex);
         
-        int total_threads = (gridDimX * blockDimX) * (gridDimY * blockDimY) * (gridDimZ * blockDimZ);
-        int grid_size = gridDimX * gridDimY * gridDimZ;
-        int block_size = blockDimX * blockDimY * blockDimZ;
+        gulong total_threads = (gridDimX * blockDimX) * (gridDimY * blockDimY) * (gridDimZ * blockDimZ);
+        gulong grid_size = gridDimX * gridDimY * gridDimZ;
+        gulong block_size = blockDimX * blockDimY * blockDimZ;
         KernelDimInfo* dim_info = g_hash_table_lookup(cuda_kernel_local_dim_mapping, g_strdup(kernel_name));
         if (!dim_info) {
             dim_info = g_new(KernelDimInfo, 1);        
@@ -213,7 +213,7 @@ static void cuda_interceptor_print_result(GHashTable* hashTable)
     g_hash_table_iter_init(&iter, hashTable);
     while (g_hash_table_iter_next(&iter, &key, &value)) {
         KernelDimInfo* dim_info = value;
-        g_printerr("|%*s|%*ld|\n",
+        g_printerr("|%*s|%*lu|\n",
             max_function_width, key,
             max_col_width, dim_info->total_kernel_call_cnt);
     }
@@ -235,7 +235,7 @@ static void cuda_interceptor_print_result(GHashTable* hashTable)
     g_hash_table_iter_init(&iter, hashTable);
     while (g_hash_table_iter_next(&iter, &key, &value)) {
         KernelDimInfo* dim_info = value;
-        g_printerr("|%*s|%*.2f|%*.2f|%*d|%*d|%*d|%*d|\n",
+        g_printerr("|%*s|%*.2f|%*.2f|%*lu|%*lu|%*lu|%*lu|\n",
             max_function_width, key,
             max_col_width, (1.0 * dim_info->total_block_size / dim_info->total_kernel_call_cnt),
             max_col_width, (1.0 * dim_info->total_grid_size / dim_info->total_kernel_call_cnt),
@@ -246,6 +246,9 @@ static void cuda_interceptor_print_result(GHashTable* hashTable)
         );
     }
     g_printerr("%.*s\n\n", row_width, row_separator);
+
+    free(space_separator);
+    free(row_separator);
 }
 
 // TODO:
@@ -267,41 +270,41 @@ cuda_interceptor_reduce_result()
         cuda_kernel_global_dim_mapping = g_hash_table_new_full(g_str_hash, str_equal_function, NULL, g_free);
     }
 
-    int local_kernel_count = g_hash_table_size(cuda_kernel_local_dim_mapping);
-    int local_values_size = local_kernel_count * sizeof(KernelDimInfo);
-    char** local_keys = g_new(char*, local_kernel_count);
+    gint local_kernel_count = g_hash_table_size(cuda_kernel_local_dim_mapping);
+    gint local_values_size = local_kernel_count * sizeof(KernelDimInfo);
+    gchar** local_keys = g_new(gchar*, local_kernel_count);
     KernelDimInfo* values = g_new(KernelDimInfo, local_kernel_count);
 
     GHashTableIter iter;
     gpointer key, value;
-    int index = 0;
-    int local_keys_buffer_size = 0;
+    guint index = 0;
+    gint local_keys_buffer_size = 0;
     g_hash_table_iter_init(&iter, cuda_kernel_local_dim_mapping);
     while (g_hash_table_iter_next(&iter, &key, &value)) {
         local_keys_buffer_size += strlen((char*) key) + 1;
-        local_keys[index] = strdup((char*) key);
+        local_keys[index] = g_strdup((char*) key);
         values[index] = *(KernelDimInfo*) value;
         index++;
     }
 
-    char* local_keys_buffer = g_new(char, local_keys_buffer_size);
-    int offset = 0;
-    for (int i = 0; i < local_kernel_count; i++) {
+    gchar* local_keys_buffer = g_new(gchar, local_keys_buffer_size);
+    guint offset = 0;
+    for (guint i = 0; i < local_kernel_count; i++) {
         strcpy(&local_keys_buffer[offset], local_keys[i]);
         offset += strlen(local_keys[i]) + 1;
     }
 
-    int global_kernel_count;
-    int global_keys_length_sum;
-    int* kernel_count_array = NULL;
-    int* keys_buffer_array = NULL;
-    int* values_buffer_array = NULL;
+    gulong global_kernel_count;
+    gulong global_keys_length_sum;
+    gulong* kernel_count_array = NULL;
+    gint* keys_buffer_array = NULL;
+    gint* values_buffer_array = NULL;
     if (world_rank == 0) {
         global_kernel_count = 0;
         global_keys_length_sum = world_size; // initialize for each rank plus one for space or \0 after words
-        kernel_count_array = malloc(world_size * sizeof(int));
-        keys_buffer_array = malloc(world_size * sizeof(int)) ;
-        values_buffer_array = malloc(world_size * sizeof(int));
+        kernel_count_array = g_new(gulong, world_size);
+        keys_buffer_array = g_new(gint, world_size);
+        values_buffer_array = g_new(gint, world_size);
     }
     MPI_Reduce(&local_kernel_count, &global_kernel_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Reduce(&local_keys_buffer_size, &global_keys_length_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -309,46 +312,47 @@ cuda_interceptor_reduce_result()
     MPI_Gather(&local_keys_buffer_size, 1, MPI_INT, keys_buffer_array, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Gather(&local_values_size, 1, MPI_INT, values_buffer_array, 1, MPI_INT, 0, MPI_COMM_WORLD);
     
-    char *global_keys_string = NULL;
-    int *keys_offset_array = NULL;
-    int *values_offset_array = NULL;
-    char **global_keys_array = NULL;
+    gchar *global_keys_string = NULL;
+    gint *keys_offset_array = NULL;
+    gint *values_offset_array = NULL;
+    gchar **global_keys_array = NULL;
     KernelDimInfo* global_values_array = NULL;
     if (world_rank == 0) {
         // Key
-        keys_offset_array = malloc(world_size * sizeof(int));
+        keys_offset_array = g_new(gint, world_size);
         keys_offset_array[0] = 0;
-        for (int i = 1; i < world_size; i++) {
+        for (guint i = 1; i < world_size; i++) {
             keys_offset_array[i] = keys_offset_array[i-1] + keys_buffer_array[i-1];
         }
-        global_keys_string = malloc(global_keys_length_sum * sizeof(char));
+        global_keys_string = g_new(gchar, global_keys_length_sum);
         memset(global_keys_string, ' ', global_keys_length_sum - 1); // allocate string, pre-fill with spaces and null terminator
         global_keys_string[global_keys_length_sum - 1] = '\0'; 
 
         // Value
         global_values_array = g_new(KernelDimInfo, global_kernel_count);
-        values_offset_array = malloc(global_kernel_count * sizeof(KernelDimInfo));
+        values_offset_array = g_new(gint, global_kernel_count);
         values_offset_array[0] = 0;
-        for (int i = 1; i < world_size; i++) {
+        for (guint i = 1; i < world_size; i++) {
             values_offset_array[i] = values_offset_array[i-1] + values_buffer_array[i-1];
         }
     }
     MPI_Gatherv(local_keys_buffer, local_keys_buffer_size, MPI_CHAR, global_keys_string, keys_buffer_array, keys_offset_array, MPI_CHAR, 0, MPI_COMM_WORLD);  
     MPI_Gatherv(values, local_kernel_count * sizeof(KernelDimInfo), MPI_BYTE, global_values_array, values_buffer_array, values_offset_array, MPI_BYTE, 0, MPI_COMM_WORLD);
 
+
     if (world_rank == 0) {
         // Spilt global key strings into global key array
-        global_keys_array = g_new(char*, global_kernel_count);
-        int index = 0;
-        for (int i = 0; i < global_kernel_count; i++) {
-            int len = strlen(global_keys_string + index);
-            global_keys_array[i] = g_new(char, len);
+        global_keys_array = g_new(gchar*, global_kernel_count);
+        guint index = 0;
+        for (guint i = 0; i < global_kernel_count; i++) {
+            guint len = strlen(global_keys_string + index);
+            global_keys_array[i] = g_new(gchar, len);
             strcpy(global_keys_array[i], global_keys_string + index);
             index += len + 1;
         }
 
         // aggregate value
-        for (int i = 0; i < global_kernel_count; i++) {
+        for (guint i = 0; i < global_kernel_count; i++) {
             KernelDimInfo* existing = (KernelDimInfo*) g_hash_table_lookup(cuda_kernel_global_dim_mapping, g_strdup(global_keys_array[i]));
             if (existing) {
                 existing->total_kernel_call_cnt += global_values_array[i].total_kernel_call_cnt;
@@ -373,16 +377,23 @@ cuda_interceptor_reduce_result()
         cuda_interceptor_print_result(cuda_kernel_global_dim_mapping);
     }
 
+    for (guint i = 0; i < local_kernel_count; i++) {
+        g_free(local_keys[i]);
+    }
     g_free(local_keys);
     g_free(values);
+    g_free(local_keys_buffer);
     if (world_rank == 0) {
         g_hash_table_destroy(cuda_kernel_global_dim_mapping);
-        free(kernel_count_array);
-        free(keys_buffer_array);
-        free(values_buffer_array);
-        free(global_keys_string);
-        free(keys_offset_array);
-        free(values_offset_array);
+        g_free(kernel_count_array);
+        g_free(keys_buffer_array);
+        g_free(values_buffer_array);
+        g_free(global_keys_string);
+        g_free(keys_offset_array);
+        g_free(values_offset_array);
+        for (guint i = 0; i < global_kernel_count; i++) {
+            g_free(global_keys_array);
+        }
         g_free(global_keys_array);
         g_free(global_values_array);
     }
