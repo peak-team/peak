bf742665a9bdffc0ace6100fa346904be9dd7524
weichenhuang <weichenhuang@login1.vista.tacc.utexas.edu>
2025-03-23 22:39:07 -0500

    feat: [CUDA] modify test and cmake list

--- test/cuda/ompmpi.sh
+++ test/cuda/ompmpi.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# Define variables
+LD_PRELOAD_PATH="../../build/src/libpeak.so"
+CUDA_LIB_PATH="/home1/apps/nvidia/Linux_aarch64/24.7/cuda/lib64"
+CUDA_INCLUDE_PATH="$CUDA_LIB_PATH/include"
+
+# Remove old files
 rm ompmpi.log test_ompmpi
-mpicxx -fopenmp -o test_ompmpi test_ompmpi.cu -L/home1/apps/nvidia/Linux_aarch64/24.7/cuda/lib64 -lcudart -I/home1/apps/nvidia/Linux_aarch64/24.7/cuda/lib64/include -fPIC --std=c++17
-LD_PRELOAD=../../build/src/libpeak.so mpirun -N 2 ./test_ompmpi --verbose --random > ompmpi.log
+
+# Compile the OpenMP MPI CUDA program
+mpicxx -fopenmp -o test_ompmpi test_ompmpi.cu -L$CUDA_LIB_PATH -lcudart -I$CUDA_INCLUDE_PATH -fPIC --std=c++17
+
+# Run the OpenMP MPI program with LD_PRELOAD
+LD_PRELOAD=$LD_PRELOAD_PATH mpirun -N 2 ./test_ompmpi --verbose --random > ompmpi.log
