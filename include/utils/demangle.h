499873a43438052930ef6330c553a5775e3e8e71
weichenhuang <weichenhuang@login1.vista.tacc.utexas.edu>
2025-03-26 00:02:26 -0500

    fix: [CUDA] fix cmake error on github CI

---
+++ include/utils/demangle.h
@@ -0,0 +1,21 @@
+#ifndef DEMANGLE_H
+#define DEMANGLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Demangles a C++ mangled symbol name.
+ *
+ * @param mangled_name The mangled symbol (e.g., "_Z3fooi").
+ * @return A newly allocated string with the demangled name.
+ *         Caller must free the returned string using free().
+ */
+char* demangle(const char* mangled_name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DEMANGLE_H
