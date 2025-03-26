4d3e2f73001e0c423aa6028295de258fce830a13
weichenhuang <weichenhuang@login1.vista.tacc.utexas.edu>
2025-03-26 00:02:26 -0500

    fix: [CUDA] fix cmake error on github CI

---
+++ src/utils/demangle.cpp
@@ -0,0 +1,19 @@
+#include "demangle.h"
+#include <cstdlib>
+#include <cstring>
+#include <cxxabi.h>
+
+char* strip_arguments(const char* demangled) {
+    char* copy = strdup(demangled);
+    char* paren = strchr(copy, '(');
+    if (paren) {
+        *paren = '\0';  // Truncate at '('
+    }
+    return copy;
+}
+
+extern "C" char* demangle(const char* mangled) {
+    int status = 0;
+    char* result = abi::__cxa_demangle(mangled, 0, 0, &status);
+    return (status == 0) ? strip_arguments(result) : strdup(mangled);
+}
