c2ac0a7bef6f2226975c4b411b2561ba15d52efe
tenngenn <1506211418@qq.com>
2026-02-25 00:39:10 -0600

    fix: Fix heartbeat-only instrumentation and ibrun command checks (#37)
    * fix: correct on-enter/on-leave logic to support heartbeat-only mode
    
    * fix: add which and seq to utils.c check_list to avoid ibrun issues
    
    * fix: Relax test_sdot PASS regex to avoid dependency on call count
    
    * test: add PEAK_HEARTBEAT_INTERVAL=0 to test environment
    
    * fix: Add heartbeat_on test

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -220,8 +220,9 @@ static const char *check_list[] = {
     "npm",
     "git",
     "ssh",
-    "scp",
     "sftp",
+    "which",
+    "seq",
     NULL // Null terminator to mark the end of the list
 };
 
