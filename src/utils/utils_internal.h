ccbae2ade57d5e7f3d3345c77bb2fa388892bbb1
wangyinz <yinzhi.wang.cug@gmail.com>
2026-07-11 16:57:00 -0500

    Make MPI parent registry resilient to PID reuse

---
+++ src/utils/utils_internal.h
@@ -0,0 +1,26 @@
+#ifndef PEAK_UTILS_INTERNAL_H
+#define PEAK_UTILS_INTERNAL_H
+
+#ifdef PEAK_UTILS_TESTING
+typedef enum {
+    PEAK_PARENT_FAULT_NONE = 0,
+    PEAK_PARENT_FAULT_PROC_READ,
+    PEAK_PARENT_FAULT_SNAPSHOT_READ,
+    PEAK_PARENT_FAULT_TEMP_WRITE,
+    PEAK_PARENT_FAULT_TEMP_FSYNC,
+    PEAK_PARENT_FAULT_RENAME,
+    PEAK_PARENT_FAULT_DIR_FSYNC,
+} PeakParentRegistryFault;
+
+typedef struct {
+    const char* proc_root;
+    const char* xdg_runtime_dir;
+    const char* tmp_root;
+    PeakParentRegistryFault fault;
+    unsigned int fault_after;
+} PeakParentRegistryTestConfig;
+
+int peak_check_parent_process_for_test(const PeakParentRegistryTestConfig* config);
+#endif
+
+#endif
